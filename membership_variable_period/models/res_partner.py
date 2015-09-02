f3008549d196241e34a9139d9893b61a9a0a93d2
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-09-02 03:01:59 +0200

    [ADD] membership_variable_period
    Variable period for memberships
    ===============================
    
    Current membership module allows to set products that define a fixed period
    for a membership. This is good when the quotas are defined periodically, and
    when you become a member, you are until the end of that quota cycle.
    
    But a lot of times, membership quotas express an amount of time that you
    gain the membership. For example, one year since the subscription.
    
    This module allows to make it in Odoo, using current membership features,
    and adapting them for this purpose. As now the quota is not attached to a fixed
    period, you can also invoice more than one quantity for being a member for
    the corresponding number of periods.
    
    Finally, a cron has been included that triggers the recalculation of the
    membership state, allowing to have "old members", which doesn't work well
    on standard.
    
    Usage
    =====
    
    Define a member product, and select 'Variable periods' in the field
    *Membership type*. You will be able to select them the period for the
    membership in quantity and interval.
    
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

---
+++ membership_variable_period/models/res_partner.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# (c) 2015 Antiun Ingeniería S.L. - Pedro M. Baeza
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from openerp import models, api
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    @api.model
+    def check_membership_expiry(self):
+        """Force a recalculation on each partner that is member."""
+        partners = self.search(
+            [('membership_state', 'not in', ['old', 'none'])])
+        # It has to be triggered one by one to avoid an error
+        for partner in partners:
+            partner.write({'membership_state': 'none'})
+        return True
