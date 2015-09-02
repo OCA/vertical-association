825056c5a1bde832600a9f925b6fe3bc3f0b419b
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-09-02 03:09:52 +0200

    [ADD] membership_initial_fee
    Initial fee for memberships
    ===========================
    
    Charge an initial fee when a partner is invoiced for the first time with a
    member product.
    
    Usage
    =====
    
    Define a member product, and select 'Fixed amount' or 'Percentage of the price'
    for invoicing an extra charge in the first invoice that is created with this
    member product.

---
+++ membership_initial_fee/models/product_template.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# (c) 2015 Antiun Ingenieria S.L. - Pedro M. Baeza
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from openerp import models, fields, api
+import openerp.addons.decimal_precision as dp
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    initial_fee = fields.Selection(
+        selection=[('none', 'No initial fee'),
+                   ('fixed', 'Fixed amount'),
+                   ('percentage', 'Percentage of the price')],
+        default='none', string="Initial fee", required=True)
+    fixed_fee = fields.Float(digits_compute=dp.get_precision('Product Price'))
+    percentage_fee = fields.Float(digits=(12, 2), string="Perc. fee (%)")
+    product_fee = fields.Many2one(
+        comodel_name='product.product', string="Product for initial fee",
+        domain="[('membership', '=', False)]")
+
+    @api.onchange('product_fee')
+    def onchange_product_fee(self):
+        self.fixed_fee = self.product_fee.list_price
