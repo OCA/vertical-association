3c80f3c54a72eafb5acf4a6e989b30395a1306b9
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-09-05 19:30:24 +0200

    membership_prorrate_variable_period
    =================================================
    Prorrate the membership fee with variable periods
    =================================================
    
    Helper module for making the prorrate correctly when you are dealing with
    variable periods in your memberships, which functionality is provided by
    the module *membership_variable_period*.
    
    Installation
    ============
    
    This module is auto-installed if you have *membership_variable_period* and
    *membership_prorrate* modules installed.
    
    Usage
    =====
    
    For leading with variable periods and prorrate, only weekly, monthly or yearly
    periods are allowed. When you create an invoice with a membership product
    with variable period, the theoretical period is calculated from the invoice
    date (or today if empty) as the natural period of the selected unit that
    includes that date. For example, if the invoice is on the 15th of January
    and the unit is month, the theoretical period to invoice will be all the month
    of January.
    
    Known issues / Roadmap
    ======================
    
    * Allow to handle periods of months that make the year divisible: 2 months
      (two-monthly), 3 months (quartely), 4 months (four-monthly) or 6 months
      (biannual).
    * On weekly memberships, consider the duality sunday/monday start of the week.

---
+++ membership_prorrate_variable_period/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# (c) 2015 Antiun Ingeniería S.L. - Pedro M. Baeza
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': 'Prorrate membership fee for variable periods',
+    'version': '1.0',
+    'category': 'Association',
+    'author': 'Antiun Ingeniería S.L., '
+              'Serv. Tecnol. Avanzados - Pedro M. Baeza, '
+              'Odoo Community Association (OCA)',
+    'website': 'http://www.antiun.com',
+    'depends': [
+        'membership_prorrate',
+        'membership_variable_period',
+    ],
+    'data': [
+    ],
+    "installable": True,
+    "auto_install": True,
+}
