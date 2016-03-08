fdf0f8f38cdf7caaa4630875e2febfd3bb03a297
Michael Viriyananda <viriyananda.michael@gmail.com>
2016-03-08 11:04:15 +0700

    Change the URL for the Issue
    Change URL for all of headers
    
    Adding Summary
    
    Add Manifest License
    
    Change URL Runbot
    
    Change Summary,split into 2 lines

--- website_membership_contact_visibility/__openerp__.py
+++ website_membership_contact_visibility/__openerp__.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 # © 2016 Michael Viriyananda
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Website Membership Contact Visibility',
     'version': '8.0.1.0.0',
+    'summary': 'Adding functionality to set '
+               'the visibility of contact information of each member',
     'author': 'Michael Viriyananda,Odoo Community Association (OCA)',
     'website': 'https://github.com/mikevhe18',
     'category': 'Website',
@@ -14,4 +16,5 @@
         'views/website_membership.xml'
     ],
     'installable': True,
+    'license': 'AGPL-3',
 }
