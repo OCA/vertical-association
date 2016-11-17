98dcec4a224053b11bdcc0b80c00b049e3e3e1f3
Antonio Espinosa <antespi@gmail.com>
2016-11-17 18:15:33 +0100

    Exclude current line when searching for other membership_lines (#9)
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- membership_variable_period/models/__init__.py
+++ membership_variable_period/models/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # (c) 2015 Antiun Ingeniería S.L. - Pedro M. Baeza
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
 from . import product_template
+from . import product_product
 from . import account_invoice
 from . import res_partner
