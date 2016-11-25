e560afd537698ff44aecb6a9bb02d5e9b94afbaf
Antonio Espinosa <antespi@gmail.com>
2016-11-25 12:16:41 +0100

    membership_extension (#21)

---
+++ membership_extension/models/product_product.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, api
+
+
+class ProductProduct(models.Model):
+    _inherit = "product.product"
+
+    @api.multi
+    def _get_next_date(self, date, qty=1):
+        self.ensure_one()
+        return self.product_tmpl_id._get_next_date(date, qty=qty)
