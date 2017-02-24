f5fd66e7ff3c4ee031b4edfa222adf33450126d3
Rigoberto Martínez <rigo1985@gmail.com>
2017-02-24 13:54:47 -0500

    [MIG] membership_initial_fee,membership_prorrate: Migrated to 10.0

--- membership_initial_fee/__manifest__.py
+++ membership_initial_fee/__manifest__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 # (c) 2015 Antiun Ingenieria S.L. - Pedro M. Baeza
+# (c) 2017 Rigoberto Martínez <rigo1985@gmail.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     'name': 'Initial fee for memberships',
-    'version': '8.0.1.0.0',
+    'version': '1.0.1',
     'license': 'AGPL-3',
     'category': 'Association',
     'author': 'Antiun Ingeniería S.L., '
@@ -17,5 +18,5 @@
     'data': [
         'views/product_template_views.xml',
     ],
-    'installable': False,
+    'installable': True,
 }
--- membership_prorrate/models/__init__.py
+++ membership_prorrate/models/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # (c) 2015 Antiun Ingenieria S.L. - Pedro M. Baeza
+# (c) 2017 Rigoberto Martínez <rigo1985@gmail.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import product_template
