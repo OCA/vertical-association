1df5e65ea47b10e50287aabb9921288de1e62dfa
Rigoberto Martínez <rigo1985@gmail.com>
2017-03-15 09:34:10 -0400

    [MIG] membership_variable_period: Migrated to 10.0

--- membership_variable_period/models/product_template.py
+++ membership_variable_period/models/product_template.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # (c) 2015 Antiun Ingeniería S.L. - Pedro M. Baeza
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from openerp import models, fields, api
+
+from odoo import models, fields, api
 from datetime import timedelta
 from dateutil.relativedelta import relativedelta
 
