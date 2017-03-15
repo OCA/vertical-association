0881772e59e5ee6a8ed363bb857c9deb2af6d2c8
Rigoberto Martínez <rigo1985@gmail.com>
2017-03-15 09:29:06 -0400

    [MIG] membership_initial_fee: Migrated to 10.0

--- membership_initial_fee/__init__.py
+++ membership_initial_fee/__init__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
-# (c) 2015 Antiun Ingenieria S.L. - Pedro M. Baeza
+# © 2015 Antiun Ingenieria S.L. - Pedro M. Baeza
+# © 2017 Rigoberto Martínez <rigo1985@gmail.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
 from . import models
--- membership_initial_fee/models/__init__.py
+++ membership_initial_fee/models/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-# (c) 2015 Antiun Ingenieria S.L. - Pedro M. Baeza
+# © 2015 Antiun Ingenieria S.L. - Pedro M. Baeza
+# © 2017 Rigoberto Martínez <rigo1985@gmail.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
 from . import product_template
 from . import account_invoice
