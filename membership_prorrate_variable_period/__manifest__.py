b88f3de74256bd0f3979d31afb061bce4c3eb825
Rigoberto Martínez <rigo1985@gmail.com>
2017-03-15 09:35:53 -0400

    [MIG] membership_prorrate_variable_period: Migrated to 10.0

---
+++ membership_prorrate_variable_period/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# (c) 2015 Antiun Ingeniería S.L. - Pedro M. Baeza
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': 'Prorrate membership fee for variable periods',
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'category': 'Association',
+    'author': 'Antiun Ingeniería S.L., '
+              'Tecnativa, '
+              'Odoo Community Association (OCA)',
+    'website': 'https://www.tecnativa.com',
+    'depends': [
+        'membership_prorrate',
+        'membership_variable_period',
+    ],
+    'data': [
+    ],
+    "installable": True,
+    "auto_install": True,
+}
