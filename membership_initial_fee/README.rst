7bafde9b571346e67c7ae849e111a8250dd61fea
David Vidal <chienandalu@gmail.com>
2017-05-17 14:40:17 +0200

    [MIG] membership_initial_fee: Migration to 10.0

--- membership_initial_fee/README.rst
+++ membership_initial_fee/README.rst
@@ -14,7 +14,7 @@ member product.
 
 .. image:: https://odoo-community.org/website/image/ir.attachment/5784_f2813bd/datas
    :alt: Try me on Runbot
-   :target: https://runbot.odoo-community.org/runbot/208/8.0
+   :target: https://runbot.odoo-community.org/runbot/208/10.0
 
 Known issues / Roadmap
 ======================
--- membership_initial_fee/models/product_template.py
+++ membership_initial_fee/models/product_template.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # (c) 2015 Antiun Ingenieria S.L. - Pedro M. Baeza
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from openerp import models, fields, api
-import openerp.addons.decimal_precision as dp
+from odoo import models, fields, api
+import odoo.addons.decimal_precision as dp
 
 
 class ProductTemplate(models.Model):
@@ -13,7 +13,7 @@ class ProductTemplate(models.Model):
                    ('fixed', 'Fixed amount'),
                    ('percentage', 'Percentage of the price')],
         default='none', string="Initial fee", required=True)
-    fixed_fee = fields.Float(digits_compute=dp.get_precision('Product Price'))
+    fixed_fee = fields.Float(digits=dp.get_precision('Product Price'))
     percentage_fee = fields.Float(digits=(12, 2), string="Perc. fee (%)")
     product_fee = fields.Many2one(
         comodel_name='product.product', string="Product for initial fee",
