775314de2758b5c9e57bc97b1f3e97b9d93c61ba
David Vidal <chienandalu@gmail.com>
2017-05-17 14:40:17 +0200

    [MIG] membership_initial_fee: Migration to 10.0

--- membership_initial_fee/models/account_invoice.py
+++ membership_initial_fee/models/account_invoice.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-# (c) 2015 Antiun Ingenieria S.L. - Pedro M. Baeza
+# Copyright 2015 Tecnativa - Pedro M. Baeza
+# Copyright 2017 Tecnativa - David Vidal
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from openerp import models, api, _
+from odoo import models, api, _
 
 
 class AccountInvoiceLine(models.Model):
@@ -16,21 +17,16 @@ class AccountInvoiceLine(models.Model):
             'invoice_id': invoice_line.invoice_id.id,
             'account_analytic_id': invoice_line.account_analytic_id.id,
         }
-        line_dict = self.product_id_change(
-            product_fee.id, False, line_vals['quantity'], '',
-            'out_invoice', invoice_line.invoice_id.partner_id.id,
-            invoice_line.invoice_id.fiscal_position.id)
-        line_vals.update(line_dict['value'])
+        inv_line = self.env['account.invoice.line'].new(line_vals)
+        inv_line._onchange_product_id()
+        inv_line.name = _('Membership initial fee')
         if product.initial_fee == 'fixed':
-            line_vals['price_unit'] = product.fixed_fee
+            inv_line.price_unit = product.fixed_fee
         elif product.initial_fee == 'percentage':
-            line_vals['price_unit'] = (
-                product.percentage_fee * invoice_line.price_unit / 100)
-        line_vals['name'] = _('Membership initial fee')
-        if line_vals.get('invoice_line_tax_id', False):
-            line_vals['invoice_line_tax_id'] = [
-                (6, 0, line_vals['invoice_line_tax_id'])]
-        return line_vals
+            inv_line.price_unit = (
+                product.percentage_fee * invoice_line.price_unit / 100
+            )
+        return inv_line._convert_to_write(inv_line._cache)
 
     @api.model
     def initial_fee_create_check(self, product, invoice_line):
