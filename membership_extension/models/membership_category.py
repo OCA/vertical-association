0c1c5e0fd220e2c7b34d21262ae64929297163d4
David Vidal <chienandalu@gmail.com>
2017-05-18 18:39:51 +0200

    [MIG] membership_extension: Migration to 10.0

--- membership_extension/models/membership_category.py
+++ membership_extension/models/membership_category.py
@@ -2,7 +2,7 @@
 # Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class MembershipCategory(models.Model):
