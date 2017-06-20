606a898df99e2433deb63eeabb478eba1a04360c
David Vidal <chienandalu@gmail.com>
2017-06-20 17:01:35 +0200

    [MIG] membership_withdrawal: Migration to 10.0

--- membership_withdrawal/__manifest__.py
+++ membership_withdrawal/__manifest__.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# Copyright 2017 David Vidal <david.vidal@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Membership withdrawal",
     "summary": "Log membership withdrawal reason and date of request",
-    "version": "8.0.1.0.0",
+    "version": "10.0.1.0.0",
     "category": "Association",
-    "website": "https://odoo-community.org/",
-    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "website": "https://odoso-community.org/",
+    "author": "Tecnativa, "
+              "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
