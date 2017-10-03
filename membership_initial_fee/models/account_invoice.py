db2c6165875a9881c4b72224e4fddb7330559727
David Vidal <chienandalu@gmail.com>
2017-10-03 14:53:14 +0200

    [MIG] membership_initial_fee: Migration to 11.0

--- membership_initial_fee/models/account_invoice.py
+++ membership_initial_fee/models/account_invoice.py
@@ -2,34 +2,23 @@
 # Copyright 2015 Tecnativa - Pedro M. Baeza
 # Copyright 2017 Tecnativa - David Vidal
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
 from odoo import models, api, _
-
-
-class AccountInvoice(models.Model):
-    _inherit = 'account.invoice'
-
-    @api.multi
-    def write(self, vals):
-        # This is a workaround for avoiding the overwriting of the invoice line
-        # Part 2: if a write operation with the context variable is detected,
-        # don't make the write - See https://github.com/odoo/odoo/pull/7971
-        if (self.env.context.get('create_membership_invoice') and
-                len(vals) == 1 and vals.get('invoice_line')):
-            return True
-        return super(AccountInvoice, self).write(vals)
 
 
 class AccountInvoiceLine(models.Model):
     _inherit = "account.invoice.line"
 
-    def _prepare_initial_fee_vals(self, invoice_line):
-        product = invoice_line.product_id
-        product_fee = invoice_line.product_id.product_fee
+    @api.multi
+    def _prepare_initial_fee_vals(self):
+        self.ensure_one()
+        product = self.product_id
+        product_fee = self.product_id.product_fee
         line_vals = {
             'product_id': product_fee.id,
             'quantity': 1.0,
-            'invoice_id': invoice_line.invoice_id.id,
-            'account_analytic_id': invoice_line.account_analytic_id.id,
+            'invoice_id': self.invoice_id.id,
+            'account_analytic_id': self.account_analytic_id.id,
         }
         inv_line = self.env['account.invoice.line'].new(line_vals)
         inv_line._onchange_product_id()
@@ -38,12 +27,12 @@ class AccountInvoiceLine(models.Model):
             inv_line.price_unit = product.fixed_fee
         elif product.initial_fee == 'percentage':
             inv_line.price_unit = (
-                product.percentage_fee * invoice_line.price_unit / 100
+                product.percentage_fee * self.price_unit / 100
             )
         return inv_line._convert_to_write(inv_line._cache)
 
-    @api.model
-    def initial_fee_create_check(self, product, invoice_line):
+    @api.multi
+    def initial_fee_create_check(self, product):
         """Inherit this method to implement a custom method
            to decide whether or not to create the initial fee
         """
@@ -52,8 +41,8 @@ class AccountInvoiceLine(models.Model):
         # See if partner has any membership line to decide whether or not
         # to create the initial fee
         member_lines = self.env['membership.membership_line'].search([
-            ('partner', '=', invoice_line.invoice_id.partner_id.id),
-            ('account_invoice_line', 'not in', (invoice_line.id, )),
+            ('partner', '=', self.invoice_id.partner_id.id),
+            ('account_invoice_line', 'not in', (self.id, )),
             ('state', 'not in', ['none', 'canceled']),
         ])
         return not bool(member_lines)
@@ -63,7 +52,7 @@ class AccountInvoiceLine(models.Model):
         invoice_line = super(AccountInvoiceLine, self).create(vals)
         product = self.env['product.product'].browse(
             vals.get('product_id'))
-        if self.initial_fee_create_check(product, invoice_line):
+        if self.initial_fee_create_check(product):
             # Charge initial fee
-            self.create(self._prepare_initial_fee_vals(invoice_line))
+            self.create(invoice_line._prepare_initial_fee_vals())
         return invoice_line
