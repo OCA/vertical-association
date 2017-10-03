494dce22d1e596fe6f62644969aec5b3098b6d63
David Vidal <chienandalu@gmail.com>
2017-10-03 14:53:14 +0200

    [MIG] membership_initial_fee: Migration to 11.0

--- membership_initial_fee/models/product_template.py
+++ membership_initial_fee/models/product_template.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-# (c) 2015 Antiun Ingenieria S.L. - Pedro M. Baeza
+# Copyright 2015 Tecnativa - Pedro M. Baeza
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
 from odoo import models, fields, api
 import odoo.addons.decimal_precision as dp
 
