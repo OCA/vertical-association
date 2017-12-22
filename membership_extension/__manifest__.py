f765b46e2cb28c08fbb524560d7ad512f0fcb781
David <david.vidal@tecnativa.com>
2017-12-22 09:57:26 +0100

    fixup! fixup! [10.0][fix] membership_extension: add security group to menu

--- membership_extension/__manifest__.py
+++ membership_extension/__manifest__.py
@@ -8,7 +8,7 @@
 {
     "name": "Membership extension",
     "summary": "Improves user experience of membership addon",
-    "version": "10.0.1.1.4",
+    "version": "10.0.1.2.0",
     "category": "Membership",
     "author": "Tecnativa, "
               "Odoo Community Association (OCA)",
