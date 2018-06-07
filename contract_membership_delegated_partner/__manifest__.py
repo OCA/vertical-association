efeb5b9ceff0e1f13d719983658b963889233895
David <david.vidal@tecnativa.com>
2018-06-07 19:02:32 +0200

    [MIG] contract_membership_delegated_partner: Migration to 11.0

--- contract_membership_delegated_partner/__manifest__.py
+++ contract_membership_delegated_partner/__manifest__.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Tecnativa - David Vidal
+# Copyright 2017-18 Tecnativa - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Contract Membership Delegate Partner',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Membership',
     'author': 'Tecnativa, '
               'Odoo Community Association (OCA)',
