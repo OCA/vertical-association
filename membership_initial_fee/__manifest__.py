e612a0520cd4e74b426f9d5e13cae9383bc3bbef
David <david.vidal@tecnativa.com>
2019-05-23 17:52:38 +0200

    [MIG] membership_initial_fee: Migration to 12.0

--- membership_initial_fee/__manifest__.py
+++ membership_initial_fee/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Tecnativa - Pedro M. Baeza
 # Copyright 2017 Tecnativa - David Vidal
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     'name': 'Initial fee for memberships',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'category': 'Association',
     'author': 'Tecnativa, '
