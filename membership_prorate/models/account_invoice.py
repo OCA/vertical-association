0a07932a91f59641388f774d7dd9b04d018d0307
David <david.vidal@tecnativa.com>
2019-05-24 17:03:53 +0200

    [MIG] membership_prorate: Migration to 12.0

--- membership_prorate/models/account_invoice.py
+++ membership_prorate/models/account_invoice.py
@@ -1,7 +1,6 @@
 # Copyright 2015 Tecnativa - Pedro M. Baeza
 # Copyright 2017-18 Tecnativa - David Vidal
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
 from odoo import models, api, fields
 from datetime import timedelta
 
@@ -17,13 +16,13 @@ class AccountInvoiceLine(models.Model):
         :return: A tuple with 2 date objects with the beginning and the
         end of the period
         """
-        date_from = fields.Date.from_string(product.membership_date_from)
-        date_to = fields.Date.from_string(product.membership_date_to)
+        date_from = product.membership_date_from
+        date_to = product.membership_date_to
         return date_from, date_to
 
     def _prepare_invoice_line_prorate_vals(self, invoice_line):
         product = invoice_line.product_id
-        date_invoice = fields.Date.from_string(
+        date_invoice = (
             invoice_line.invoice_id.date_invoice or fields.Date.today())
         date_from, date_to = self._get_membership_interval(
             product, date_invoice)
