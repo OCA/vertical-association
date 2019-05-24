5fd33d40fcbe1197da654d5b7d7617c9739caadd
David <david.vidal@tecnativa.com>
2019-05-24 17:03:53 +0200

    [MIG] membership_prorate: Migration to 12.0

--- membership_prorate/__manifest__.py
+++ membership_prorate/__manifest__.py
@@ -1,10 +1,9 @@
 # Copyright 2015 Tecnativa - Pedro M. Baeza
 # Copyright 2017 Tecnativa - David Vidal
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
 {
     'name': 'Prorate membership fee',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'category': 'Association',
     'author': 'Tecnativa, '
