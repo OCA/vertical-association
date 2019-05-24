7f70e248aeac972b3f0773b8aabb7aa203bff6b1
David <david.vidal@tecnativa.com>
2019-05-24 09:59:22 +0200

    [MIG] membership_withdrawal: Migration to 12.0
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- membership_withdrawal/models/res_partner.py
+++ membership_withdrawal/models/res_partner.py
@@ -1,6 +1,5 @@
-# Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
+# Copyright 2016 Tecnativa  - Antonio Espinosa
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import api, fields, models
 
 
