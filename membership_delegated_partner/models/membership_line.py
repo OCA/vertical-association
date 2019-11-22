e6ee3696f9c5d0a6bd611ba507de647f840b9664
Andrea <a.stirpe@onestein.nl>
2019-11-22 12:25:05 +0100

    pre-commit

--- membership_delegated_partner/models/membership_line.py
+++ membership_delegated_partner/models/membership_line.py
@@ -2,17 +2,18 @@
 # Copyright 2019 Onestein - Andrea Stirpe
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, models, fields
+from odoo import api, fields, models
 
 
 class MembershipLine(models.Model):
-    _inherit = 'membership.membership_line'
+    _inherit = "membership.membership_line"
 
-    partner = fields.Many2one(
-        compute='_compute_partner', store=True, readonly=False)
+    partner = fields.Many2one(compute="_compute_partner", store=True, readonly=False)
 
-    @api.depends('account_invoice_line.move_id.delegated_member_id',
-                 'account_invoice_line.move_id.partner_id')
+    @api.depends(
+        "account_invoice_line.move_id.delegated_member_id",
+        "account_invoice_line.move_id.partner_id",
+    )
     def _compute_partner(self):
         """Change associated membership lines if delegated member is changed.
         """
@@ -24,24 +25,23 @@ class MembershipLine(models.Model):
     @api.model
     def create(self, vals):
         """ Delegate the member line to the designated partner """
-        if 'account_invoice_line' not in vals:
+        if "account_invoice_line" not in vals:
             return super().create(vals)
-        line = self.env['account.move.line'].browse(
-            vals['account_invoice_line'])
+        line = self.env["account.move.line"].browse(vals["account_invoice_line"])
         if line.move_id.delegated_member_id:
-            vals['partner'] = line.move_id.delegated_member_id.id
+            vals["partner"] = line.move_id.delegated_member_id.id
         return super().create(vals)
 
     def write(self, vals):
         """If a partner is delegated, avoid reassign"""
-        if 'partner' not in vals:
+        if "partner" not in vals:
             return super().write(vals)
-        if vals.get('account_invoice_line'):
-            inv_line = self.env['account.move.line'].browse(
-                vals['account_invoice_line']
+        if vals.get("account_invoice_line"):
+            inv_line = self.env["account.move.line"].browse(
+                vals["account_invoice_line"]
             )
         else:
             inv_line = self.account_invoice_line
         if inv_line and inv_line.move_id.delegated_member_id:
-            vals['partner'] = inv_line.move_id.delegated_member_id.id
+            vals["partner"] = inv_line.move_id.delegated_member_id.id
         return super().write(vals)
