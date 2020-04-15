7f3b6580316820d26306b9b7c0d7a9a5f7b5e539
Andrea <a.stirpe@onestein.nl>
2020-04-15 14:42:23 +0200

    [13.0][MIG] membership_extension

--- membership_extension/models/product_template.py
+++ membership_extension/models/product_template.py
@@ -1,6 +1,6 @@
 # Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
 # Copyright 2019 Onestein - Andrea Stirpe
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from datetime import timedelta
 
@@ -13,10 +13,11 @@ class ProductTemplate(models.Model):
     membership_category_id = fields.Many2one(
         string="Membership category",
         comodel_name="membership.membership_category",
-        domain="['|',('company_id','=',False),('company_id','=',company_id)]",
+        compute="_compute_membership_category_id",
+        readonly=False,
+        store=True,
     )
 
-    @api.multi
     def _get_next_date(self, date, qty=1):
         self.ensure_one()
         if self.membership_date_to:
@@ -24,10 +25,10 @@ class ProductTemplate(models.Model):
             return date_to + timedelta(1)
         return False  # pragma: no cover
 
-    @api.onchange("company_id")
-    def _onchange_company_id_compute_membership_category_id(self):
+    @api.depends("company_id")
+    def _compute_membership_category_id(self):
         """Reset the Membership Category in case a different Company is set"""
-        category_company = self.membership_category_id.company_id
-        if self.company_id and category_company:
-            if not category_company == self.company_id:
-                self.membership_category_id = False
+        for record in self:
+            if record.company_id and record.membership_category_id.company_id:
+                if record.membership_category_id.company_id != record.company_id:
+                    record.membership_category_id = False
