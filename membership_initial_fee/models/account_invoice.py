7509232e0960507c44f94b3c4852f96a10fed7f9
Andrea <a.stirpe@onestein.nl>
2020-08-04 09:02:19 +0200

    pre-commit

--- membership_initial_fee/models/account_invoice.py
+++ membership_initial_fee/models/account_invoice.py
@@ -15,21 +15,18 @@ class AccountInvoiceLine(models.Model):
         product = self.product_id
         product_fee = self.product_id.product_fee
         line_vals = {
-            'product_id': product_fee.id,
-            'quantity': 1.0,
-            'invoice_id': self.invoice_id.id,
-            'account_analytic_id': self.account_analytic_id.id,
+            "product_id": product_fee.id,
+            "quantity": 1.0,
+            "invoice_id": self.invoice_id.id,
+            "account_analytic_id": self.account_analytic_id.id,
         }
-        inv_line = self.env['account.invoice.line'].new(line_vals)
+        inv_line = self.env["account.invoice.line"].new(line_vals)
         inv_line._onchange_product_id()
-        inv_line.name = (product_fee.description_sale or
-                         _('Membership initial fee'))
-        if product.initial_fee == 'fixed':
+        inv_line.name = product_fee.description_sale or _("Membership initial fee")
+        if product.initial_fee == "fixed":
             inv_line.price_unit = product.fixed_fee
-        elif product.initial_fee == 'percentage':
-            inv_line.price_unit = (
-                product.percentage_fee * self.price_unit / 100
-            )
+        elif product.initial_fee == "percentage":
+            inv_line.price_unit = product.percentage_fee * self.price_unit / 100
         return inv_line._convert_to_write(inv_line._cache)
 
     @api.multi
@@ -43,16 +40,17 @@ class AccountInvoiceLine(models.Model):
         """
         # TODO: remove product parameter in v12
         product = product or self.product_id
-        if not product or not product.membership or (
-                product.initial_fee == 'none'):
+        if not product or not product.membership or (product.initial_fee == "none"):
             return False
         # See if partner has any membership line to decide whether or not
         # to create the initial fee
-        member_lines = self.env['membership.membership_line'].search([
-            ('partner', '=', self.invoice_id.partner_id.id),
-            ('account_invoice_line', 'not in', (self.id,)),
-            ('state', 'not in', ['none', 'canceled']),
-        ])
+        member_lines = self.env["membership.membership_line"].search(
+            [
+                ("partner", "=", self.invoice_id.partner_id.id),
+                ("account_invoice_line", "not in", (self.id,)),
+                ("state", "not in", ["none", "canceled"]),
+            ]
+        )
         return not bool(member_lines)
 
     @api.model
