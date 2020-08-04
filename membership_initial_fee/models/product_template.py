57aed23c0768b5f4c9ffc593771c41b7723be9b0
Andrea <a.stirpe@onestein.nl>
2020-08-04 09:04:21 +0200

    [13.0][MIG] membership_initial_fee

--- membership_initial_fee/models/product_template.py
+++ membership_initial_fee/models/product_template.py
@@ -1,9 +1,8 @@
 # Copyright 2015 Tecnativa - Pedro M. Baeza
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+# Copyright 2019 Onestein - Andrea Stirpe
+# License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0
 
 from odoo import api, fields, models
-
-import odoo.addons.decimal_precision as dp
 
 
 class ProductTemplate(models.Model):
@@ -19,7 +18,9 @@ class ProductTemplate(models.Model):
         string="Initial fee",
         required=True,
     )
-    fixed_fee = fields.Float(digits=dp.get_precision("Product Price"))
+    fixed_fee = fields.Float(
+        compute="_compute_fixed_fee", readonly=False, store=True, digits="Product Price"
+    )
     percentage_fee = fields.Float(digits=(12, 2), string="Perc. fee (%)")
     product_fee = fields.Many2one(
         comodel_name="product.product",
@@ -27,6 +28,7 @@ class ProductTemplate(models.Model):
         domain="[('membership', '=', False)]",
     )
 
-    @api.onchange("product_fee")
-    def onchange_product_fee(self):
-        self.fixed_fee = self.product_fee.list_price
+    @api.depends("product_fee.list_price")
+    def _compute_fixed_fee(self):
+        for template in self:
+            template.fixed_fee = template.product_fee.list_price
