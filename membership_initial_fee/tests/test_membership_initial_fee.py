768a73740e7af40b6711cdfb4bf9bbb8b2ac29a7
Andrea <a.stirpe@onestein.nl>
2020-08-04 09:04:21 +0200

    [13.0][MIG] membership_initial_fee

--- membership_initial_fee/tests/test_membership_initial_fee.py
+++ membership_initial_fee/tests/test_membership_initial_fee.py
@@ -1,6 +1,6 @@
 # Copyright 2015 Tecnativa - Pedro M. Baeza
 # Copyright 2017 Tecnativa - David Vidal
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+# License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0
 
 from odoo.tests import common
 
@@ -8,10 +8,10 @@ from odoo.tests import common
 class TestMembershipInitialFee(common.SavepointCase):
     @classmethod
     def setUpClass(cls):
-        super(TestMembershipInitialFee, cls).setUpClass()
-        cls.product_category = cls.env["product.category"].create({"name": "test_cat",})
+        super().setUpClass()
+        cls.product_category = cls.env["product.category"].create({"name": "test_cat"})
         cls.product = cls.env["product.product"].create(
-            {"name": "Test product", "list_price": 100,}
+            {"name": "Test product", "list_price": 100}
         )
         cls.product_fee = cls.env["product.product"].create(
             {
@@ -49,7 +49,7 @@ class TestMembershipInitialFee(common.SavepointCase):
                 "membership_date_to": "2017-02-01",
             }
         )
-        cls.partner = cls.env["res.partner"].create({"name": "Test",})
+        cls.partner = cls.env["res.partner"].create({"name": "Test"})
 
     def check_membership_invoice(self, invoice, expected_amount):
         self.assertEqual(
@@ -66,46 +66,40 @@ class TestMembershipInitialFee(common.SavepointCase):
 
     def test_create_invoice_wo_initial_fee(self):
         self.product_fixed.initial_fee = "none"
-        invoice_id = self.partner.create_membership_invoice(
-            product_id=self.product_fixed.id, datas={"amount": 1.0}
-        )
-        invoice = self.env["account.invoice"].browse(invoice_id)
+        invoice = self.partner.create_membership_invoice(self.product_fixed, 1.0)
         self.assertEqual(
             len(invoice.invoice_line_ids), 1, "The created invoice should have 1 lines"
         )
 
     def test_create_invoice_initial_fee_fixed(self):
-        invoice_id = self.partner.create_membership_invoice(
-            product_id=self.product_fixed.id, datas={"amount": 1.0}
-        )
-        invoice = self.env["account.invoice"].browse(invoice_id)
+        invoice = self.partner.with_context(
+            check_move_validity=False
+        ).create_membership_invoice(self.product_fixed, 1.0)
         self.check_membership_invoice(invoice, 50.0)
 
     def test_create_invoice_initial_fee_percentage(self):
-        invoice_id = self.partner.create_membership_invoice(
-            product_id=self.product_percentage.id, datas={"amount": 150.0}
-        )[0]
-        invoice = self.env["account.invoice"].browse(invoice_id)
+        invoice = self.partner.with_context(
+            check_move_validity=False
+        ).create_membership_invoice(self.product_percentage, 150.0)
         self.check_membership_invoice(invoice, 15.0)
 
     def test_create_invoice_initial_fee_taxes(self):
         tax = self.env["account.tax"].create(
-            {"name": "Tax", "amount_type": "percent", "amount": 0.10,}
+            {"name": "Tax", "amount_type": "percent", "amount": 0.10}
         )
         self.product_fixed.product_fee.taxes_id = tax
-        invoice_id = self.partner.create_membership_invoice(
-            product_id=self.product_fixed.id, datas={"amount": 150.0},
-        )[0]
-        invoice = self.env["account.invoice"].browse(invoice_id)
+        invoice = self.partner.with_context(
+            check_move_validity=False
+        ).create_membership_invoice(self.product_fixed, 150.0)
         initial_fee_line = invoice.invoice_line_ids.filtered(
             lambda x: x.product_id == self.product_fee
         )
-        self.assertEqual(initial_fee_line.invoice_line_tax_ids, tax)
+        initial_fee_line._onchange_product_id()
+        self.assertEqual(initial_fee_line.tax_ids, tax)
 
     def test_onchange_product_fee(self):
         membership_product = self.env["product.template"].new(
-            {"name": "Test membership product",}
+            {"name": "Test membership product"}
         )
         membership_product.product_fee = self.product
-        membership_product.onchange_product_fee()
         self.assertEqual(membership_product.fixed_fee, self.product.list_price)
