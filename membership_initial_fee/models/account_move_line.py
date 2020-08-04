7231864195602ed9be242e92912cf88246d0e1f2
Andrea <a.stirpe@onestein.nl>
2020-08-04 09:04:21 +0200

    [13.0][MIG] membership_initial_fee

--- membership_initial_fee/models/account_move_line.py
+++ membership_initial_fee/models/account_move_line.py
@@ -1,27 +1,27 @@
 # Copyright 2015 Tecnativa - Pedro M. Baeza
 # Copyright 2017 Tecnativa - David Vidal
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+# Copyright 2019 Onestein - Andrea Stirpe
+# License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0
 
 from odoo import api, models
 from odoo.tools.translate import _
 
 
-class AccountInvoiceLine(models.Model):
-    _inherit = "account.invoice.line"
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
 
-    @api.multi
     def _prepare_initial_fee_vals(self):
         self.ensure_one()
         product = self.product_id
-        product_fee = self.product_id.product_fee
+        product_fee = product.product_fee
         line_vals = {
             "product_id": product_fee.id,
             "quantity": 1.0,
-            "invoice_id": self.invoice_id.id,
-            "account_analytic_id": self.account_analytic_id.id,
+            "move_id": self.move_id.id,
+            "account_id": self.account_id.id,
+            "analytic_account_id": self.analytic_account_id.id,
         }
-        inv_line = self.env["account.invoice.line"].new(line_vals)
-        inv_line._onchange_product_id()
+        inv_line = self.env["account.move.line"].new(line_vals)
         inv_line.name = product_fee.description_sale or _("Membership initial fee")
         if product.initial_fee == "fixed":
             inv_line.price_unit = product.fixed_fee
@@ -29,34 +29,33 @@ class AccountInvoiceLine(models.Model):
             inv_line.price_unit = product.percentage_fee * self.price_unit / 100
         return inv_line._convert_to_write(inv_line._cache)
 
-    @api.multi
-    def initial_fee_create_check(self, product=False):
+    def initial_fee_create_check(self):
         """
         Inherit this method to implement a custom method
         to decide whether or not to create the initial fee
 
-        :param product:
         :return:
         """
-        # TODO: remove product parameter in v12
-        product = product or self.product_id
-        if not product or not product.membership or (product.initial_fee == "none"):
+        self.ensure_one()
+        product = self.product_id
+        if not product or not product.membership or product.initial_fee == "none":
             return False
         # See if partner has any membership line to decide whether or not
         # to create the initial fee
         member_lines = self.env["membership.membership_line"].search(
             [
-                ("partner", "=", self.invoice_id.partner_id.id),
+                ("partner", "=", self.move_id.partner_id.id),
                 ("account_invoice_line", "not in", (self.id,)),
                 ("state", "not in", ["none", "canceled"]),
             ]
         )
         return not bool(member_lines)
 
-    @api.model
-    def create(self, vals):
-        invoice_line = super(AccountInvoiceLine, self).create(vals)
-        if invoice_line.initial_fee_create_check():
-            # Charge initial fee
-            self.create(invoice_line._prepare_initial_fee_vals())
-        return invoice_line
+    @api.model_create_multi
+    def create(self, vals_list):
+        move_lines = super().create(vals_list)
+        for move_line in move_lines:
+            if move_line.move_id.is_invoice() and move_line.initial_fee_create_check():
+                # Charge initial fee
+                self.create([move_line._prepare_initial_fee_vals()])
+        return move_lines
