2a507b62851672f537a9f2725cd83f5ed4d9280d
Andrea <a.stirpe@onestein.nl>
2020-08-12 13:37:52 +0200

    pre-commit
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- membership_variable_period/__manifest__.py
+++ membership_variable_period/__manifest__.py
@@ -3,18 +3,13 @@
 # Copyright 2017 Tecnativa - David Vidal
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
-    'name': 'Variable period for memberships',
-    'version': '12.0.1.0.1',
-    'license': 'AGPL-3',
-    'category': 'Association',
-    'author': 'Tecnativa, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/vertical-association',
-    'depends': [
-        'membership_extension',
-    ],
-    'data': [
-        'views/product_template_views.xml',
-    ],
+    "name": "Variable period for memberships",
+    "version": "12.0.1.0.1",
+    "license": "AGPL-3",
+    "category": "Association",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/vertical-association",
+    "depends": ["membership_extension",],
+    "data": ["views/product_template_views.xml",],
     "installable": True,
 }
