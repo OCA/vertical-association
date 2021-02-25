1cae527e8aa0e5adac3dd069dc2d4048c1977e4e
miguels73 <55379877+miguels73@users.noreply.github.com>
2021-02-25 18:59:02 +0100

    [MIG] contract_membership_delegated_partner: Migration to 13.0

--- contract_membership_delegated_partner/__manifest__.py
+++ contract_membership_delegated_partner/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Contract Membership Delegate Partner",
-    "version": "12.0.1.0.1",
+    "version": "13.0.1.0.0",
     "development_status": "Production/Stable",
     "category": "Membership",
     "author": "Tecnativa, " "Odoo Community Association (OCA)",
