627f04392bf8a179b3d4ce89b7454c23d0b724ae
miguels73 <55379877+miguels73@users.noreply.github.com>
2021-02-25 18:34:08 +0100

    [IMP] contract_membership_delegated_partner: black, isort, prettier

--- contract_membership_delegated_partner/__manifest__.py
+++ contract_membership_delegated_partner/__manifest__.py
@@ -1,21 +1,15 @@
 # Copyright 2017-18 Tecnativa - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Contract Membership Delegate Partner',
-    'version': '12.0.1.0.1',
+    "name": "Contract Membership Delegate Partner",
+    "version": "12.0.1.0.1",
     "development_status": "Production/Stable",
-    'category': 'Membership',
-    'author': 'Tecnativa, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/oca/vertical-association',
-    'license': 'AGPL-3',
-    'summary': 'Set delegate membership on the contract',
-    'depends': [
-        'membership_delegated_partner',
-        'contract',
-    ],
-    'data': [
-        'views/contract.xml',
-    ],
-    'installable': True,
+    "category": "Membership",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/oca/vertical-association",
+    "license": "AGPL-3",
+    "summary": "Set delegate membership on the contract",
+    "depends": ["membership_delegated_partner", "contract"],
+    "data": ["views/contract.xml"],
+    "installable": True,
 }
