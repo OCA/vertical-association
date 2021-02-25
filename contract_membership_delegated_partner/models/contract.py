0770ac002497f66be39af5ab5007f36887b1f151
miguels73 <55379877+miguels73@users.noreply.github.com>
2021-02-25 18:34:08 +0100

    [IMP] contract_membership_delegated_partner: black, isort, prettier

--- contract_membership_delegated_partner/models/contract.py
+++ contract_membership_delegated_partner/models/contract.py
@@ -4,14 +4,13 @@ from odoo import fields, models
 
 
 class AccountAnalyticAccount(models.Model):
-    _inherit = 'contract.contract'
+    _inherit = "contract.contract"
 
     delegated_member_id = fields.Many2one(
-        comodel_name='res.partner',
-        string='Delegated Member',
+        comodel_name="res.partner", string="Delegated Member",
     )
 
     def _prepare_invoice(self, date_invoice, journal=None):
         res = super()._prepare_invoice(date_invoice, journal=journal)
-        res['delegated_member_id'] = self.delegated_member_id.id
+        res["delegated_member_id"] = self.delegated_member_id.id
         return res
