63742a2f5721b995ef645b44ac63eb32f6f1982f
Víctor Martínez <victor.martinez@tecnativa.com>
2021-06-03 10:58:37 +0200

    [MIG] membership_delegated_partner: Migration to 14.0

--- membership_delegated_partner/__manifest__.py
+++ membership_delegated_partner/__manifest__.py
@@ -3,9 +3,9 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Membership Delegate Partner",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Membership",
-    "author": "Tecnativa, " "Onestein, " "Odoo Community Association (OCA)",
+    "author": "Tecnativa, Onestein, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/vertical-association",
     "license": "AGPL-3",
     "summary": "Delegate membership on a specific partner",
