6df6dfba1af6fb9ed9dbf955f246ae621ccb645f
Víctor Martínez <victor.martinez@tecnativa.com>
2021-06-03 10:47:43 +0200

    [IMP] membership_delegated_partner: black, isort, prettier

--- membership_delegated_partner/__manifest__.py
+++ membership_delegated_partner/__manifest__.py
@@ -6,7 +6,7 @@
     "version": "13.0.1.0.0",
     "category": "Membership",
     "author": "Tecnativa, " "Onestein, " "Odoo Community Association (OCA)",
-    "website": "https://github.com/oca/vertical-association",
+    "website": "https://github.com/OCA/vertical-association",
     "license": "AGPL-3",
     "summary": "Delegate membership on a specific partner",
     "depends": ["membership"],
