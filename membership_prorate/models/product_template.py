9a8112c4bf4ca68ab4064c6c44e15ae531a8a145
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2022-01-21 12:30:30 -0500

    [IMP] membership_prorate: black, isort, prettier

--- membership_prorate/models/product_template.py
+++ membership_prorate/models/product_template.py
@@ -1,6 +1,6 @@
 # Copyright 2015 Tecnativa - Pedro M. Baeza
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ProductTemplate(models.Model):
@@ -9,6 +9,6 @@ class ProductTemplate(models.Model):
     membership_prorate = fields.Boolean(
         string="Prorate",
         help="If this check is marked, then the fee will be proportionally "
-             "charged for the remaining time of the period",
+        "charged for the remaining time of the period",
         oldname="membership_prorrate",
     )
