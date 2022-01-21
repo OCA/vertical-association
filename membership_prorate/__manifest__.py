13b9b6d68c43b287d5d9095e2e27596e8e24d833
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2022-01-21 12:30:30 -0500

    [IMP] membership_prorate: black, isort, prettier

--- membership_prorate/__manifest__.py
+++ membership_prorate/__manifest__.py
@@ -2,18 +2,17 @@
 # Copyright 2017 Tecnativa - David Vidal
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
-    'name': 'Prorate membership fee',
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'category': 'Association',
-    'author': 'Tecnativa, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/vertical-association',
-    'depends': [
-        'membership',
+    "name": "Prorate membership fee",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Association",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/vertical-association",
+    "depends": [
+        "membership",
     ],
-    'data': [
-        'views/product_template_views.xml',
+    "data": [
+        "views/product_template_views.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
