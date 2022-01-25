b046f31341ff493f3cd2b9cda2b9bf641385a635
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2022-01-25 12:34:54 -0500

    [MIG] membership_prorate: Migration to 14.0

--- membership_prorate/tests/test_membership_prorate.py
+++ membership_prorate/tests/test_membership_prorate.py
@@ -1,14 +1,16 @@
 # Copyright 2015 Tecnativa - Pedro M. Baeza
 # Copyright 2017-19 Tecnativa - David Vidal
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from odoo import fields
-from odoo.tests import common
+from datetime import date
+
+from odoo.tests import Form, common
 
 
-class TestMembershipProrate(common.TransactionCase):
-    def setUp(self):
-        super(TestMembershipProrate, self).setUp()
-        self.product = self.env["product.product"].create(
+class TestMembershipProrate(common.SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.product = cls.env["product.product"].create(
             {
                 "name": "Membership product with prorate",
                 "membership": True,
@@ -17,35 +19,44 @@ class TestMembershipProrate(common.TransactionCase):
                 "membership_date_to": "2017-12-31",
             }
         )
-        self.partner = self.env["res.partner"].create({"name": "Test"})
+        receivable_type = cls.env["account.account.type"].create(
+            {
+                "name": "Test receivable account",
+                "type": "receivable",
+                "internal_group": "income",
+            }
+        )
+        receivable = cls.env["account.account"].create(
+            {
+                "name": "Test receivable account",
+                "code": "TEST_RA",
+                "user_type_id": receivable_type.id,
+                "reconcile": True,
+            }
+        )
+        cls.partner = cls.env["res.partner"].create(
+            {
+                "name": "Test",
+                "property_account_receivable_id": receivable.id,
+            }
+        )
 
     def test_create_invoice_membership_product_wo_prorate(self):
         self.product.membership_prorate = False
-        invoice_id = self.partner.create_membership_invoice(
-            product_id=self.product.id, datas={"amount": 1.0}
-        )
-        invoice = self.env["account.invoice"].browse(invoice_id)
+        invoice = self.partner.create_membership_invoice(self.product, 1.0)
         self.assertEqual(invoice.invoice_line_ids[0].quantity, 1.0)
 
     def test_create_invoice_membership_product_prorate(self):
-        account = self.partner.property_account_receivable_id
-        invoice = self.env["account.invoice"].create(
-            {
-                "partner_id": self.partner.id,
-                "date_invoice": "2017-07-01",
-                "account_id": account.id,
-            }
+        invoice_form = Form(
+            self.env["account.move"].with_context(default_move_type="out_invoice")
         )
-        self.env["account.invoice.line"].create(
-            {
-                "product_id": self.product.id,
-                "price_unit": 100.0,
-                "name": "Membership with prorate",
-                "account_id": account.id,
-                "invoice_id": invoice.id,
-                "quantity": 1.0,
-            }
-        )
+        invoice_form.partner_id = self.partner
+        invoice_form.invoice_date = date(2017, 7, 1)
+        with invoice_form.invoice_line_ids.new() as line_form:
+            line_form.product_id = self.product
+            line_form.price_unit = 100.0
+            line_form.tax_ids.clear()
+        invoice = invoice_form.save()
         # Result is rounded to 2 decimals for avoiding the fail in tests
         # if "Product Unit of Measure" precision changes in the future
         self.assertAlmostEqual(invoice.invoice_line_ids[0].quantity, 0.50, 2)
@@ -53,42 +64,32 @@ class TestMembershipProrate(common.TransactionCase):
             [("account_invoice_line", "=", invoice.invoice_line_ids[0].id)], limit=1
         )
         self.assertAlmostEqual(memb_line.member_price, 50.00, 2)
-        self.assertEqual(memb_line.date_from, fields.Date.from_string("2017-07-01"))
+        self.assertEqual(memb_line.date_from, date(2017, 7, 1))
         # Set the date six months before the membership period
-        invoice.date_invoice = "2016-07-01"
-        self.env["account.invoice.line"].create(
-            {
-                "product_id": self.product.id,
-                "price_unit": 100.0,
-                "name": "Membership with prorate",
-                "account_id": account.id,
-                "invoice_id": invoice.id,
-                "quantity": 1.0,
-            }
-        )
+        with Form(invoice) as invoice_form:
+            invoice_form.invoice_date = date(2016, 7, 1)
+            with invoice_form.invoice_line_ids.new() as line_form:
+                line_form.product_id = self.product
+                line_form.price_unit = 100.0
+                line_form.tax_ids.clear()
         # The whole period is calculated
         self.assertAlmostEqual(invoice.invoice_line_ids[1].quantity, 1.0, 2)
         memb_line = self.env["membership.membership_line"].search(
             [("account_invoice_line", "=", invoice.invoice_line_ids[1].id)], limit=1
         )
         self.assertAlmostEqual(memb_line.member_price, 100.00, 2)
-        self.assertEqual(memb_line.date_from, fields.Date.from_string("2017-01-01"))
+        self.assertEqual(memb_line.date_from, date(2017, 1, 1))
         # Set the date six months after the membership period
-        invoice.date_invoice = "2018-07-01"
-        self.env["account.invoice.line"].create(
-            {
-                "product_id": self.product.id,
-                "price_unit": 100.0,
-                "name": "Membership with prorate",
-                "account_id": account.id,
-                "invoice_id": invoice.id,
-                "quantity": 1.0,
-            }
-        )
+        with Form(invoice) as invoice_form:
+            invoice_form.invoice_date = date(2018, 7, 1)
+            with invoice_form.invoice_line_ids.new() as line_form:
+                line_form.product_id = self.product
+                line_form.price_unit = 100.0
+                line_form.tax_ids.clear()
         # Nothing to invoice
         self.assertAlmostEqual(invoice.invoice_line_ids[2].quantity, 0, 2)
         memb_line = self.env["membership.membership_line"].search(
             [("account_invoice_line", "=", invoice.invoice_line_ids[2].id)], limit=1
         )
         self.assertAlmostEqual(memb_line.member_price, 0.00, 2)
-        self.assertEqual(memb_line.date_from, fields.Date.from_string("2017-12-31"))
+        self.assertEqual(memb_line.date_from, date(2017, 12, 31))
