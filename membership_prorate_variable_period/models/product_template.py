50cb9185f45e6320fd6c5b52882d9e850874d357
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2022-01-27 19:59:15 -0500

    [IMP] membership_prorate_variable_period: black, isort, prettier

--- membership_prorate_variable_period/models/product_template.py
+++ membership_prorate_variable_period/models/product_template.py
@@ -2,6 +2,7 @@
 # Copyright 2019 Tecnativa - David Vidal
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 import math
+
 from odoo import _, api, exceptions, models
 from odoo.tools import date_utils
 
@@ -12,17 +13,16 @@ class ProductTemplate(models.Model):
     @api.multi
     def _get_next_date(self, date, qty=1):
         next_date = super(ProductTemplate, self)._get_next_date(date)
-        if self.membership_interval_unit == 'days':
-            raise exceptions.Warning(
-                _("It's not possible to prorate daily periods."))
+        if self.membership_interval_unit == "days":
+            raise exceptions.Warning(_("It's not possible to prorate daily periods."))
         qty = math.ceil(qty) * self.membership_interval_qty
-        if self.membership_interval_unit == 'weeks':
-            next_date = date_utils.start_of(date, 'week')
+        if self.membership_interval_unit == "weeks":
+            next_date = date_utils.start_of(date, "week")
             next_date = date_utils.add(next_date, weeks=qty)
-        elif self.membership_interval_unit == 'months':
-            next_date = date_utils.start_of(date, 'month')
+        elif self.membership_interval_unit == "months":
+            next_date = date_utils.start_of(date, "month")
             next_date = date_utils.add(next_date, months=qty)
-        elif self.membership_interval_unit == 'years':
-            next_date = date_utils.start_of(date, 'year')
+        elif self.membership_interval_unit == "years":
+            next_date = date_utils.start_of(date, "year")
             next_date = date_utils.add(next_date, years=qty)
         return next_date
