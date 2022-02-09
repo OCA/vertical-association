f996f6ab9880890bfe230675bf3efc6bd4b5b6d0
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2022-02-09 12:10:31 -0500

    [MIG] membership_prorate_variable_period: Migration to 14.0

--- membership_prorate_variable_period/__manifest__.py
+++ membership_prorate_variable_period/__manifest__.py
@@ -3,10 +3,10 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Prorate membership fee for variable periods",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "category": "Association",
-    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/vertical-association",
     "depends": [
         "membership_prorate",
