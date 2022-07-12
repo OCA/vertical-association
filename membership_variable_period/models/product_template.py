cca4645518714e90cc4f60a6be5713206f173c16
Robin Keunen <robin@keunen.net>
2022-07-12 18:40:16 +0200

    [MIG] membership_variable_period: migration to 16.0.1.0.0
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- membership_variable_period/models/product_template.py
+++ membership_variable_period/models/product_template.py
@@ -40,18 +40,17 @@ class ProductTemplate(models.Model):
     membership_type = fields.Selection(
         selection=[("fixed", "Fixed dates"), ("variable", "Variable periods")],
         default="fixed",
-        string="Membership type",
         required=True,
     )
     membership_interval_qty = fields.Integer(string="Interval quantity", default=1)
     membership_interval_unit = fields.Selection(
+        string="Interval Unit",
         selection=[
             ("days", "days"),
             ("weeks", "weeks"),
             ("months", "months"),
             ("years", "years"),
         ],
-        string="Interval unit",
         default="years",
     )
 
@@ -60,10 +59,15 @@ class ProductTemplate(models.Model):
             vals["membership_date_from"] = vals["membership_date_to"] = False
         return vals
 
-    @api.model
-    def create(self, vals):
-        self._correct_vals_membership_type(vals, vals.get("membership_type", "fixed"))
-        return super().create(vals)
+    @api.model_create_multi
+    def create(self, vals_list):
+        fixed_vals_list = []
+        for vals in vals_list:
+            fixed_vals = vals.copy()
+            membership_type = fixed_vals.get("membership_type", "fixed")
+            fixed_vals = self._correct_vals_membership_type(fixed_vals, membership_type)
+            fixed_vals_list.append(fixed_vals)
+        return super().create(fixed_vals_list)
 
     def write(self, vals):
         if not vals.get("membership_type"):
