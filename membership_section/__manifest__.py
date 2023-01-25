ea01c97065e42611f68d67507f76ec21b7d3584f
antonio <espo-tony@hotmail.it>
2023-01-25 23:27:09 +0100

    Beautify code and fix tests

--- membership_section/__manifest__.py
+++ membership_section/__manifest__.py
@@ -1,11 +1,11 @@
 # Copyright 2023 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": 'Membership Section',
+    "name": "Membership Section",
     "description": "Add Sections to the Membership functional area.",
-    "category": 'Membership',
-    "version": '15.0.1.0.0',
-    "author": 'Onestein, Odoo Community Association (OCA)',
+    "category": "Membership",
+    "version": "15.0.1.0.0",
+    "author": "Onestein, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/vertical-association",
     "depends": [
--- membership_section/models/membership_section.py
+++ membership_section/models/membership_section.py
@@ -2,46 +2,40 @@ from odoo import api, fields, models
 
 
 class MembershipSection(models.Model):
-    _name = 'membership.section'
-    _description = 'Membership Section'
+    _name = "membership.section"
+    _description = "Membership Section"
 
     name = fields.Char()
     section_membership_ids = fields.One2many(
-        'membership.section.membership',
-        'section_id'
+        "membership.section.membership", "section_id"
     )
     partner_ids = fields.Many2many(
-        'res.partner',
-        string='Contacts',
-        compute='_compute_partner_ids'
+        "res.partner", string="Contacts", compute="_compute_partner_ids"
     )
     partner_ids_count = fields.Integer(
-        '# of Members',
-        compute='_compute_partner_ids'
+        "# of Members", compute="_compute_partner_ids"
     )
 
     @api.depends(
-        'section_membership_ids',
-        'section_membership_ids.partner_id'
+        "section_membership_ids", "section_membership_ids.partner_id"
     )
     def _compute_partner_ids(self):
         for section in self:
-            section.partner_ids = section.section_membership_ids.mapped(
-                'partner_id')
+            section.partner_ids = section.section_membership_ids.mapped("partner_id")
             section.partner_ids_count = len(section.partner_ids)
 
     def action_open_partner_view(self):
-        action = self.env.ref('membership.action_membership_members')
+        action = self.env.ref("membership.action_membership_members")
         result = action.read()[0]
 
-        result['context'] = {}
+        result["context"] = {}
         record_ids = sum([record.partner_ids.ids for record in self], [])
         if len(record_ids) > 1:
-            result['domain'] = "[('id','in',[" + ','.join(
+            result["domain"] = "[('id','in',[" + ",".join(
                 map(str, record_ids)) + "])]"
         elif len(record_ids) == 1:
             res = self.env.ref(
-                'base.view_partner_form', False)
-            result['views'] = [(res and res.id or False, 'form')]
-            result['res_id'] = (record_ids and record_ids[0]) or False
+                "base.view_partner_form", False)
+            result["views"] = [(res and res.id or False, "form")]
+            result["res_id"] = (record_ids and record_ids[0]) or False
         return result
