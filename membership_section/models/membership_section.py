95e5a40046bef76ff66614517b7cd901a88ecdf4
antonio <espo-tony@hotmail.it>
2023-01-25 23:37:38 +0100

    Beautify code

--- membership_section/models/membership_section.py
+++ membership_section/models/membership_section.py
@@ -12,13 +12,9 @@ class MembershipSection(models.Model):
     partner_ids = fields.Many2many(
         "res.partner", string="Contacts", compute="_compute_partner_ids"
     )
-    partner_ids_count = fields.Integer(
-        "# of Members", compute="_compute_partner_ids"
-    )
+    partner_ids_count = fields.Integer("# of Members", compute="_compute_partner_ids")
 
-    @api.depends(
-        "section_membership_ids", "section_membership_ids.partner_id"
-    )
+    @api.depends("section_membership_ids", "section_membership_ids.partner_id")
     def _compute_partner_ids(self):
         for section in self:
             section.partner_ids = section.section_membership_ids.mapped("partner_id")
@@ -29,13 +25,11 @@ class MembershipSection(models.Model):
         result = action.read()[0]
 
         result["context"] = {}
-        record_ids = sum([record.partner_ids.ids for record in self], [])
+        record_ids = sum((record.partner_ids.ids for record in self), [])
         if len(record_ids) > 1:
-            result["domain"] = "[('id','in',[" + ",".join(
-                map(str, record_ids)) + "])]"
+            result["domain"] = "[('id','in',[" + ",".join(map(str, record_ids)) + "])]"
         elif len(record_ids) == 1:
-            res = self.env.ref(
-                "base.view_partner_form", False)
+            res = self.env.ref("base.view_partner_form", False)
             result["views"] = [(res and res.id or False, "form")]
             result["res_id"] = (record_ids and record_ids[0]) or False
         return result
