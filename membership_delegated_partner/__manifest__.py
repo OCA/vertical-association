8714374653825e5297527b34f0ec82771e0734af
David <david.vidal@tecnativa.com>
2023-03-22 13:15:47 +0100

    [MIG] membership_delegated_partner: Migration to 15.0
    TT42042

--- membership_delegated_partner/__manifest__.py
+++ membership_delegated_partner/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Membership Delegate Partner",
-    "version": "14.0.1.1.0",
+    "version": "15.0.1.0.0",
     "category": "Membership",
     "author": "Tecnativa, Onestein, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/vertical-association",
