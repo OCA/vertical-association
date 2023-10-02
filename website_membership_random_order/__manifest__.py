1e3a8dd625b98b40adfd44fe40f5ab1556b2f040
Carolina Fernandez <carolina.fernandez@tecnativa.com>
2023-10-02 06:40:33 -0300

    [MIG] website_membership_random_order: Migration to 16.0

--- website_membership_random_order/__manifest__.py
+++ website_membership_random_order/__manifest__.py
@@ -1,8 +1,9 @@
 # Copyright 2021 Tecnativa - Pedro M. Baeza
+# Copyright 2023 Tecnativa - Carolina Fernandez
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Online Members Directory - Random order",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Association",
     "website": "https://github.com/OCA/vertical-association",
     "author": "Tecnativa, Odoo Community Association (OCA)",
