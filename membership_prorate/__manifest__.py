888db1cf21179dd4eff1a1021baa9d8297fb05a9
anjeel.haria <anjeel.haria@brainvire.com>
2023-12-20 18:16:54 +0530

    [MIG] membership_prorate: Migration to 16.0
    Fix test scripts
    
    Fix test scripts

--- membership_prorate/__manifest__.py
+++ membership_prorate/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Prorate membership fee",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.1",
     "license": "AGPL-3",
     "category": "Association",
     "author": "Tecnativa, Odoo Community Association (OCA)",
@@ -14,5 +14,4 @@
     "data": [
         "views/product_template_views.xml",
     ],
-    "installable": True,
 }
