fee19801fca093e62356002390bd65b67cf87192
Víctor Martínez <victor.martinez@tecnativa.com>
2024-11-15 16:43:34 +0100

    [MIG] website_membership_random_order: Migration to 17.0
    TT51560

--- website_membership_random_order/models/membership_line.py
+++ website_membership_random_order/models/membership_line.py
@@ -9,7 +9,7 @@ from odoo import models
 class MembershipLine(models.Model):
     _inherit = "membership.membership_line"
 
-    def search(self, args, offset=0, limit=None, order=None, count=False):
+    def search(self, domain, offset=0, limit=None, order=None):
         """If we detect the injected context, and only when the search is done with
         limit (as there are 2 calls to search in the controller), we search for all
         the results, randomize them, and return them. We can't do slices, as if not, we
@@ -17,9 +17,7 @@ class MembershipLine(models.Model):
         random results.
         """
         if self.env.context.get("random_membership_line_order") and limit:
-            record_ids = super().search(args, limit=None).ids
+            record_ids = super().search(domain, limit=None).ids
             random.shuffle(record_ids)
             return self.browse(record_ids)
-        return super().search(
-            args, offset=offset, limit=limit, order=order, count=count
-        )
+        return super().search(domain, offset=offset, limit=limit, order=order)
