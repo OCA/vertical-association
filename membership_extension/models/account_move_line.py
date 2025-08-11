2ddc55b6e32fbebf809b155073c4f31747a92c92
Cleber Santz <clebersantz@gmail.com>
2025-08-11 08:20:36 -0300

    remove Member free membership when Invoice is created

--- membership_extension/models/account_move_line.py
+++ membership_extension/models/account_move_line.py
@@ -3,8 +3,6 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
-import logging
-_logger = logging.getLogger(__name__)
 
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
@@ -21,7 +19,6 @@ class AccountMoveLine(models.Model):
                 line.membership_lines.write({"state": "waiting"})
                 #line.membership_lines.mapped("partner").write({"free_member": False})
                 for partner in line.membership_lines.mapped("partner"):
-                    _logger.info("Atualizando parceiro: %s", partner.id)
                     partner.write({"free_member": False})
         return lines
 
