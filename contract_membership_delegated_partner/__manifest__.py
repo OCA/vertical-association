59c0954524362f8fef594f5b8eeaef12e9d2ae8a
Andrii Kompaniiets <andrii@moduon.team>
2025-08-20 13:26:42 +0200

    [MIG] contract_membership_delegated_partner: Migration to 18.0

--- contract_membership_delegated_partner/__manifest__.py
+++ contract_membership_delegated_partner/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Contract Membership Delegate Partner",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "development_status": "Production/Stable",
     "category": "Membership",
     "author": "Tecnativa, " "Odoo Community Association (OCA)",
