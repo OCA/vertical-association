c51caf9c76aa56326c61df7560e6960c20d6a049
Simone Rubino <simone.rubino@pytech.it>
2026-04-15 10:45:38 +0200

    [ADD] crm_membership

---
+++ crm_membership/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2026 Simone Rubino - PyTech
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Membership data in CRM",
+    "summary": "Shows membership data in CRM",
+    "version": "18.0.1.0.0",
+    "category": "Membership",
+    "author": "PyTech, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/vertical-association",
+    "license": "AGPL-3",
+    "maintainers": [
+        "SirPyTech",
+    ],
+    "depends": [
+        "crm",
+        "membership",
+    ],
+    "data": [
+        "views/crm_lead_views.xml",
+    ],
+}
---
+++ crm_membership/readme/DESCRIPTION.md
@@ -0,0 +1,1 @@
+Show Membership information in CRM Lead/Opportunity views.
